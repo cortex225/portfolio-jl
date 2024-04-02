1c53092c5a4a20b94ad25a29d4d7e40117407a25
jlgouaho <1900751@CegepGranby.qc.ca>
2024-04-02 11:14:26 -0400

    update

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,8 @@ import About from "@/components/About";
 import Services from "@/components/Services";
 import Work from "@/components/Work";
 import Cta from "@/components/Cta";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
     return <main>
